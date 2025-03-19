b2a1ec6828698b17725afe07f505b7050e5275f5
satyam382003 <140716707+Satyam382003@users.noreply.github.com>
2025-03-19 06:26:09 +0000

    update BMI

--- 02_BMI Generator/BMI.js
+++ 02_BMI Generator/BMI.js
@@ -13,7 +13,7 @@ form.addEventListener("submit", function (e) {
   } else if (weight === "" || weight < 0 || isNaN(weight)) {
     results.innerHTML = `pls give a valid weight ${weight}`;
   } else {
-    const bmi = (weight / ((height * height) / 1000)).toFixed(3);
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
     //show the results
     results.innerHTML = `<span>${bmi}</span>`;
   }
